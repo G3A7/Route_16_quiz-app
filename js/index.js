0b819b0fe7bc988d556719e42edbdba30351cec7
Ahmed <mnaz004587@gmail.com>
2024-12-23 23:21:36 +0200

    .

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ let buttonPrev = document.querySelector("#Prev");
 let buttonNext = document.querySelector("#Next");
 let quizSection = document.querySelector(".quiz");
 let home = document.querySelector(".home");
+let questionEl = document.querySelector(".num-qu");
+const boxMove = document.querySelector(".box-move");
+const innerTime = document.querySelector(".inner-time");
 let selected = "";
 let difficulty = "";
 let start = 0;
@@ -35,15 +38,39 @@ async function getData(url) {
   home.classList.replace("d-block", "d-none");
   assign(results);
   display(questionData[0], 0);
+  showNumberQ(0);
   console.log(questionData);
+  // Timer  ;
+}
+
+function showNumberQ(s) {
+  let blackBox = "";
+  questionData.forEach((e, idx) => {
+    blackBox += `  <div data-num="${idx}" class="box ${idx == s ? "active" : ""}">${idx + 1}</div>`;
+  });
+  boxMove.innerHTML = blackBox;
+
+  boxMove.querySelectorAll(".box").forEach((e) => {
+    e.addEventListener("click", (num) => {
+      boxMove.querySelectorAll(".box").forEach((e) => {
+        e.classList.remove("active");
+      });
+      num.target.classList.add("active");
+      start = +num.target.dataset.num;
+      s = start;
+      questionEl.innerHTML = `Question ${s + 1} of ${questionData.length}`;
+
+      display(questionData[s], s);
+    });
+  });
 }
 
 function display(questionObj, id) {
   let ans = [];
   ans = [...questionObj.incorrect_answers, questionObj.correct_answer].sort();
-  let blackBox = `<p class="num-qu w-50">Number Questions : <span>${questionData.length}</span></p>
-              <p class="num-qu">Current Questions : <span>${start + 1}</span></p>
-              <h2 id="question" class="mt-3 text-start">${questionObj?.question}</h2>
+  questionEl.innerHTML = `Question ${id + 1} of ${questionData.length}`;
+  let blackBox = `
+              <h2 id="question" class="mt-3 ">${questionObj?.question}</h2>
               <ul class="my-3 py-3" data-numqu=${id} id="options">
                 <li  data-num="0" class='${questionObj.num == 0 ? "active" : ""}'><span>${
     ans[0]
@@ -107,6 +134,7 @@ buttonNext.addEventListener("click", (e) => {
     start++;
     display(questionData[start], start);
     buttonPrev.classList.remove("opacity-50-cursor");
+    showNumberQ(start);
   } else {
     buttonNext.classList.add("opacity-50-cursor");
   }
@@ -115,6 +143,8 @@ buttonPrev.addEventListener("click", (e) => {
   if (start > 0) {
     start--;
     display(questionData[start], start);
+    showNumberQ(start);
+
     buttonNext.classList.remove("opacity-50-cursor");
   } else {
     buttonPrev.classList.add("opacity-50-cursor");
